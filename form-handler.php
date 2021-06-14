4ed0f74b04bc6018342319c606152c7fd7b9e97b
Vinicius-Rodrigues51 <vinicius.motta.09@hotmail.com>
2021-06-14 20:14:35 -0300

    first commit

---
+++ form-handler.php
@@ -0,0 +1,28 @@
+<?php
+
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'UniversetyLondon@gmail.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name./n".
+                "User Email $visitor_email./n".
+                "Subject: $subject./n".
+                "User Message: $message./n";
+
+$to = 'vinimma@live.com';
+
+$headers = "From: $email_from /r/n";
+
+$headers .= "Reply-to $visitor_email /r/n";
+
+mail($to,$email_subject,$email_body, $headers);
+
+header("Location: contato.html");
+
+
+?>
